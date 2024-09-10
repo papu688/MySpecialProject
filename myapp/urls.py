21e20ea3f25912b2f11534d04f7928715fbc753c
papu_68 <giorgobianipaul@gmail.com>
2024-09-10 13:25:39 +0200

    created swagger for API

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,3 +5,8 @@ from . import views
 router = DefaultRouter()
 router.register(r'authors', views.AuthorViewSet)
 router.register(r'books', views.BookViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls))
+]
--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,18 @@
 from rest_framework import viewsets
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
