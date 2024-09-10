ce5b065e72cce9a356d3ee786864fae604a36d32
papu_68 <giorgobianipaul@gmail.com>
2024-09-10 12:12:48 +0200

    added serializers for Author and Bookmodels

--- myapp/models.py
+++ myapp/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
-class Bool(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=100)
     AUTHOR = models.ForeignKey(Author, on_delete=models.CASCADE)
